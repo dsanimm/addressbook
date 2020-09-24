8f921e68598bf7266d31f3e4e6451e13d5443e2f
dsanimm <dsanimm@gmail.com>
2020-09-25 00:51:11 +0530

    testcase3

--- javaproject/src/javaproject/AddBookSys.java
+++ javaproject/src/javaproject/AddBookSys.java
@@ -13,8 +13,14 @@ public class AddBookSys {
 		long phoneNo;
 		AddContactArray add = new AddContactArray();
 
-		
-			
+		while(choice!=4) {
+			System.out.println("Enter your choice:\n1. Enter contact\n2. Edit Contact\n3. Exit");
+
+			choice = sc.nextInt();
+
+
+			switch(choice) {
+			case 1:
 				System.out.println("Enter First Name:");
 				 firstName = sc.next();
 				System.out.println("Enter Last Name:");
@@ -27,12 +33,38 @@ public class AddBookSys {
 				 phoneNo=sc.nextLong();
 				System.out.println("Enter email address:");
 				 email=sc.next();
-				add.addContact(firstName, lastName, city, zip, phoneNo, 				email);
+				add.addContact(firstName, lastName, city, zip, phoneNo, email);
 				System.out.println("Record Added");
 				break;
+			case 2:
+				System.out.println("Enter First Name:");
+				 firstName = sc.next();
+
+				if(add.containsKey(firstName)) {
+					System.out.println("Enter Last Name:");
+					 lastName=sc.next();
+					System.out.println("Enter city:");
+					 city=sc.next();
+					System.out.println("Enter zip:");
+					 zip=sc.nextInt();
+					System.out.println("Enter phone No.:");
+					 phoneNo=sc.nextInt();
+					System.out.println("Enter email address:");
+					 email=sc.next();
+				add.editContact(firstName, lastName, city, zip, phoneNo, email);
+				}
+				else System.out.println("Record Not Found");
+				
+				
+				break;
 			
+			case 3:
+				break;
+			default: System.out.println("Wrong choice");
+				
 				
 		
-		
+		}
+	}
 	}
 }
--- javaproject/src/javaproject/AddContactArray.java
+++ javaproject/src/javaproject/AddContactArray.java
@@ -4,11 +4,21 @@ import java.util.HashMap;
 
 public class AddContactArray {
 	private HashMap<String,AddressContact> arr= new HashMap<String,AddressContact>();
-	
+	public boolean containsKey(String firstName) {
+		if (this.arr.containsKey(firstName))
+		return true;
+		else return false;
+	}
 	public void addContact(String firstName,String lastName,String city,int zip, long phoneNo, String email) {
 		AddressContact add =new AddressContact(firstName,lastName,city,zip,phoneNo,email);
 		arr.put(firstName,add);
 	}
+	public void editContact(String firstName,String lastName,String city,int zip, long phoneNo, String email) {
+		AddressContact add =new AddressContact(firstName,lastName,city,zip,phoneNo,email);
+		arr.replace(firstName, add);
+		System.out.println("Record Changed");
+		
+		}
 	
 
 }
