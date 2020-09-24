84943fe187d541b4bf0746c0dd085964e0c4f028
dsanimm <dsanimm@gmail.com>
2020-09-25 00:46:24 +0530

    testcase1

--- javaproject/src/javaproject/AddBookSys.java
+++ javaproject/src/javaproject/AddBookSys.java
@@ -1,10 +1,22 @@
 package javaproject;
 
-
+import java.util.Scanner;
 
 public class AddBookSys {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
-			}
+		Scanner sc=new Scanner(System.in);		
+		
+		String firstName,lastName,city,email;
+		int zip;
+		long phoneNo;
+		AddressContact add =new AddressContact(firstName,lastName,city,zip,phoneNo,email);
+				
+				
+				
+		
+		}
+	}
+	}
 }
---
+++ javaproject/src/javaproject/AddressContact.java
@@ -0,0 +1,19 @@
+package javaproject;
+
+public class AddressContact {
+	public String firstName;
+	public String lastName;
+	public String city;
+	public int zip;
+	public long phoneNo;
+	public String email;
+	
+public AddressContact(String firstName,String lastName,String city,int zip, long phoneno, String email) {
+	this.firstName=firstName;
+	this.lastName=lastName;
+	this.city=city;
+	this.zip=zip;
+	this.phoneNo=phoneNo;
+	this.email=email;
+}
+}
